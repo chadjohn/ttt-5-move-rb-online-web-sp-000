7b23b3e373a8a4283285754c5b6d9990e10ddc63
Chad Johnson <chad@chadjohn.com>
2020-12-02 02:35:59 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,7 +8,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-#display_board(board)
+display_board(board)
 #input_to_index
 def input_to_index(user_input)
   user_input.to_i - 1
